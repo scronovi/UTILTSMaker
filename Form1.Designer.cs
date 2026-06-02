1c741fb65cf62997b939a30fda3d695db591e87b
scronovi <33103889+scronovi@users.noreply.github.com>
2026-06-02 22:11:43 +0200

    .

---
+++ Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace UTILTSMaker;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        inputBox = new System.Windows.Forms.TextBox();
+        uniqueIdTextBox = new System.Windows.Forms.TextBox();
+        dateFromPicker = new System.Windows.Forms.DateTimePicker();
+        uniqueIdLabel = new System.Windows.Forms.Label();
+        dateFromLabel = new System.Windows.Forms.Label();
+        dateToLabel = new System.Windows.Forms.Label();
+        dateToPicker = new System.Windows.Forms.DateTimePicker();
+        timeSeries1 = new System.Windows.Forms.CheckBox();
+        timeSeries15 = new System.Windows.Forms.CheckBox();
+        consumtionAp = new System.Windows.Forms.CheckBox();
+        productionAp = new System.Windows.Forms.CheckBox();
+        netOwnerLabel = new System.Windows.Forms.Label();
+        netIdTextBox = new System.Windows.Forms.TextBox();
+        netAreaTextBox = new System.Windows.Forms.TextBox();
+        netAreaLabel = new System.Windows.Forms.Label();
+        createFileButton = new System.Windows.Forms.Button();
+        outputPathLabel = new System.Windows.Forms.Label();
+        outputPathTextBox = new System.Windows.Forms.TextBox();
+        outputPathButton = new System.Windows.Forms.Button();
+        saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+        accountingPointTextBox = new System.Windows.Forms.TextBox();
+        accountPointIdLabel = new System.Windows.Forms.Label();
+        documentTimeFromNetTextBox = new System.Windows.Forms.TextBox();
+        timeFromNetLabel = new System.Windows.Forms.Label();
+        meteringLabel = new System.Windows.Forms.Label();
+        meteringTo = new System.Windows.Forms.TextBox();
+        meteringFrom = new System.Windows.Forms.TextBox();
+        meteringTotalBox = new System.Windows.Forms.TextBox();
+        SuspendLayout();
+        // 
+        // inputBox
+        // 
+        inputBox.Location = new System.Drawing.Point(21, 22);
+        inputBox.Multiline = true;
+        inputBox.Name = "inputBox";
+        inputBox.PlaceholderText = "Input values..";
+        inputBox.Size = new System.Drawing.Size(336, 656);
+        inputBox.TabIndex = 0;
+        // 
+        // uniqueIdTextBox
+        // 
+        uniqueIdTextBox.Location = new System.Drawing.Point(363, 42);
+        uniqueIdTextBox.Name = "uniqueIdTextBox";
+        uniqueIdTextBox.ReadOnly = true;
+        uniqueIdTextBox.Size = new System.Drawing.Size(404, 23);
+        uniqueIdTextBox.TabIndex = 2;
+        uniqueIdTextBox.TextChanged += uniqueIdTextBox_TextChanged;
+        // 
+        // dateFromPicker
+        // 
+        dateFromPicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+        dateFromPicker.Location = new System.Drawing.Point(363, 94);
+        dateFromPicker.Name = "dateFromPicker";
+        dateFromPicker.RightToLeft = System.Windows.Forms.RightToLeft.No;
+        dateFromPicker.Size = new System.Drawing.Size(404, 23);
+        dateFromPicker.TabIndex = 3;
+        // 
+        // uniqueIdLabel
+        // 
+        uniqueIdLabel.Location = new System.Drawing.Point(363, 19);
+        uniqueIdLabel.Name = "uniqueIdLabel";
+        uniqueIdLabel.Size = new System.Drawing.Size(404, 23);
+        uniqueIdLabel.TabIndex = 4;
+        uniqueIdLabel.Text = "Unique ID:";
+        // 
+        // dateFromLabel
+        // 
+        dateFromLabel.Location = new System.Drawing.Point(363, 68);
+        dateFromLabel.Name = "dateFromLabel";
+        dateFromLabel.Size = new System.Drawing.Size(404, 23);
+        dateFromLabel.TabIndex = 5;
+        dateFromLabel.Text = "Datum från:";
+        // 
+        // dateToLabel
+        // 
+        dateToLabel.Location = new System.Drawing.Point(363, 120);
+        dateToLabel.Name = "dateToLabel";
+        dateToLabel.Size = new System.Drawing.Size(404, 23);
+        dateToLabel.TabIndex = 6;
+        dateToLabel.Text = "Datum till:";
+        // 
+        // dateToPicker
+        // 
+        dateToPicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+        dateToPicker.Location = new System.Drawing.Point(363, 146);
+        dateToPicker.Name = "dateToPicker";
+        dateToPicker.Size = new System.Drawing.Size(404, 23);
+        dateToPicker.TabIndex = 7;
+        // 
+        // timeSeries1
+        // 
+        timeSeries1.Location = new System.Drawing.Point(363, 175);
+        timeSeries1.Name = "timeSeries1";
+        timeSeries1.Size = new System.Drawing.Size(206, 21);
+        timeSeries1.TabIndex = 10;
+        timeSeries1.Text = "Månadsavläst (1)";
+        timeSeries1.UseVisualStyleBackColor = true;
+        timeSeries1.CheckedChanged += timeSeries1_CheckedChanged;
+        // 
+        // timeSeries15
+        // 
+        timeSeries15.Location = new System.Drawing.Point(363, 202);
+        timeSeries15.Name = "timeSeries15";
+        timeSeries15.Size = new System.Drawing.Size(206, 21);
+        timeSeries15.TabIndex = 11;
+        timeSeries15.Text = "Kvartsavläst (15)";
+        timeSeries15.UseVisualStyleBackColor = true;
+        timeSeries15.CheckedChanged += timeSeries15_CheckedChanged;
+        // 
+        // consumtionAp
+        // 
+        consumtionAp.Location = new System.Drawing.Point(575, 175);
+        consumtionAp.Name = "consumtionAp";
+        consumtionAp.Size = new System.Drawing.Size(192, 21);
+        consumtionAp.TabIndex = 12;
+        consumtionAp.Text = "Konsumtion";
+        consumtionAp.UseVisualStyleBackColor = true;
+        consumtionAp.CheckedChanged += consumtionAp_CheckedChanged;
+        // 
+        // productionAp
+        // 
+        productionAp.Location = new System.Drawing.Point(575, 202);
+        productionAp.Name = "productionAp";
+        productionAp.Size = new System.Drawing.Size(192, 21);
+        productionAp.TabIndex = 13;
+        productionAp.Text = "Produktion";
+        productionAp.UseVisualStyleBackColor = true;
+        productionAp.CheckedChanged += productionAp_CheckedChanged;
+        // 
+        // netOwnerLabel
+        // 
+        netOwnerLabel.Location = new System.Drawing.Point(363, 226);
+        netOwnerLabel.Name = "netOwnerLabel";
+        netOwnerLabel.Size = new System.Drawing.Size(404, 23);
+        netOwnerLabel.TabIndex = 15;
+        netOwnerLabel.Text = "Nätägare:";
+        // 
+        // netIdTextBox
+        // 
+        netIdTextBox.Location = new System.Drawing.Point(363, 254);
+        netIdTextBox.Name = "netIdTextBox";
+        netIdTextBox.PlaceholderText = "e.g. \"49100\"";
+        netIdTextBox.Size = new System.Drawing.Size(403, 23);
+        netIdTextBox.TabIndex = 16;
+        // 
+        // netAreaTextBox
+        // 
+        netAreaTextBox.Location = new System.Drawing.Point(363, 308);
+        netAreaTextBox.Name = "netAreaTextBox";
+        netAreaTextBox.PlaceholderText = "e.g. \"UMO\"";
+        netAreaTextBox.Size = new System.Drawing.Size(403, 23);
+        netAreaTextBox.TabIndex = 18;
+        // 
+        // netAreaLabel
+        // 
+        netAreaLabel.Location = new System.Drawing.Point(363, 280);
+        netAreaLabel.Name = "netAreaLabel";
+        netAreaLabel.Size = new System.Drawing.Size(404, 23);
+        netAreaLabel.TabIndex = 17;
+        netAreaLabel.Text = "Nätområde:";
+        // 
+        // createFileButton
+        // 
+        createFileButton.Location = new System.Drawing.Point(363, 604);
+        createFileButton.Name = "createFileButton";
+        createFileButton.Size = new System.Drawing.Size(403, 74);
+        createFileButton.TabIndex = 23;
+        createFileButton.Text = "Skapa";
+        createFileButton.UseVisualStyleBackColor = true;
+        createFileButton.Click += createFileButton_Click;
+        // 
+        // outputPathLabel
+        // 
+        outputPathLabel.Location = new System.Drawing.Point(363, 551);
+        outputPathLabel.Name = "outputPathLabel";
+        outputPathLabel.Size = new System.Drawing.Size(403, 23);
+        outputPathLabel.TabIndex = 20;
+        outputPathLabel.Text = "Output-katalog:";
+        // 
+        // outputPathTextBox
+        // 
+        outputPathTextBox.Location = new System.Drawing.Point(363, 575);
+        outputPathTextBox.Name = "outputPathTextBox";
+        outputPathTextBox.Size = new System.Drawing.Size(328, 23);
+        outputPathTextBox.TabIndex = 21;
+        // 
+        // outputPathButton
+        // 
+        outputPathButton.Location = new System.Drawing.Point(697, 575);
+        outputPathButton.Name = "outputPathButton";
+        outputPathButton.Size = new System.Drawing.Size(69, 23);
+        outputPathButton.TabIndex = 22;
+        outputPathButton.Text = "Bläddra";
+        outputPathButton.UseVisualStyleBackColor = true;
+        outputPathButton.Click += outputPathButton_Click;
+        // 
+        // accountingPointTextBox
+        // 
+        accountingPointTextBox.Location = new System.Drawing.Point(363, 362);
+        accountingPointTextBox.Name = "accountingPointTextBox";
+        accountingPointTextBox.PlaceholderText = "e.g. \"735999111222333444\"";
+        accountingPointTextBox.Size = new System.Drawing.Size(403, 23);
+        accountingPointTextBox.TabIndex = 19;
+        // 
+        // accountPointIdLabel
+        // 
+        accountPointIdLabel.Location = new System.Drawing.Point(363, 334);
+        accountPointIdLabel.Name = "accountPointIdLabel";
+        accountPointIdLabel.Size = new System.Drawing.Size(404, 23);
+        accountPointIdLabel.TabIndex = 24;
+        accountPointIdLabel.Text = "Anläggnings-ID:";
+        // 
+        // documentTimeFromNetTextBox
+        // 
+        documentTimeFromNetTextBox.Location = new System.Drawing.Point(363, 416);
+        documentTimeFromNetTextBox.Name = "documentTimeFromNetTextBox";
+        documentTimeFromNetTextBox.PlaceholderText = "e.g. \"202605250945\" (yyyyMMddHHmm)";
+        documentTimeFromNetTextBox.Size = new System.Drawing.Size(403, 23);
+        documentTimeFromNetTextBox.TabIndex = 25;
+        // 
+        // timeFromNetLabel
+        // 
+        timeFromNetLabel.Location = new System.Drawing.Point(363, 388);
+        timeFromNetLabel.Name = "timeFromNetLabel";
+        timeFromNetLabel.Size = new System.Drawing.Size(404, 23);
+        timeFromNetLabel.TabIndex = 26;
+        timeFromNetLabel.Text = "Skapande-tid (från nät):";
+        // 
+        // meteringLabel
+        // 
+        meteringLabel.Location = new System.Drawing.Point(363, 442);
+        meteringLabel.Name = "meteringLabel";
+        meteringLabel.Size = new System.Drawing.Size(404, 23);
+        meteringLabel.TabIndex = 28;
+        meteringLabel.Text = "Mätarställning:";
+        meteringLabel.Visible = false;
+        // 
+        // meteringTo
+        // 
+        meteringTo.Location = new System.Drawing.Point(575, 470);
+        meteringTo.Name = "meteringTo";
+        meteringTo.PlaceholderText = "mätarställning slut";
+        meteringTo.Size = new System.Drawing.Size(191, 23);
+        meteringTo.TabIndex = 30;
+        meteringTo.Visible = false;
+        meteringTo.TextChanged += meteringTo_TextChanged;
+        // 
+        // meteringFrom
+        // 
+        meteringFrom.Location = new System.Drawing.Point(363, 470);
+        meteringFrom.Name = "meteringFrom";
+        meteringFrom.PlaceholderText = "mätarställning start";
+        meteringFrom.Size = new System.Drawing.Size(191, 23);
+        meteringFrom.TabIndex = 29;
+        meteringFrom.Visible = false;
+        meteringFrom.TextChanged += meteringFrom_TextChanged;
+        // 
+        // meteringTotalBox
+        // 
+        meteringTotalBox.Location = new System.Drawing.Point(363, 499);
+        meteringTotalBox.Name = "meteringTotalBox";
+        meteringTotalBox.PlaceholderText = "total förbrukning";
+        meteringTotalBox.ReadOnly = true;
+        meteringTotalBox.Size = new System.Drawing.Size(403, 23);
+        meteringTotalBox.TabIndex = 31;
+        meteringTotalBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+        meteringTotalBox.Visible = false;
+        // 
+        // Form1
+        // 
+        ClientSize = new System.Drawing.Size(794, 704);
+        Controls.Add(meteringTotalBox);
+        Controls.Add(meteringFrom);
+        Controls.Add(meteringTo);
+        Controls.Add(meteringLabel);
+        Controls.Add(documentTimeFromNetTextBox);
+        Controls.Add(timeFromNetLabel);
+        Controls.Add(accountingPointTextBox);
+        Controls.Add(accountPointIdLabel);
+        Controls.Add(outputPathButton);
+        Controls.Add(outputPathTextBox);
+        Controls.Add(outputPathLabel);
+        Controls.Add(createFileButton);
+        Controls.Add(netAreaTextBox);
+        Controls.Add(netAreaLabel);
+        Controls.Add(netIdTextBox);
+        Controls.Add(netOwnerLabel);
+        Controls.Add(productionAp);
+        Controls.Add(consumtionAp);
+        Controls.Add(timeSeries15);
+        Controls.Add(timeSeries1);
+        Controls.Add(dateToPicker);
+        Controls.Add(dateToLabel);
+        Controls.Add(dateFromLabel);
+        Controls.Add(uniqueIdLabel);
+        Controls.Add(dateFromPicker);
+        Controls.Add(uniqueIdTextBox);
+        Controls.Add(inputBox);
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+        MaximizeBox = false;
+        ShowIcon = false;
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    private System.Windows.Forms.TextBox meteringTotalBox;
+
+    private System.Windows.Forms.Label meteringLabel;
+    private System.Windows.Forms.TextBox meteringTo;
+    private System.Windows.Forms.TextBox meteringFrom;
+
+    private System.Windows.Forms.TextBox documentTimeFromNetTextBox;
+    private System.Windows.Forms.Label timeFromNetLabel;
+
+    private System.Windows.Forms.TextBox accountingPointTextBox;
+    private System.Windows.Forms.Label accountPointIdLabel;
+
+    private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+    private System.Windows.Forms.Button createFileButton;
+    private System.Windows.Forms.Label outputPathLabel;
+    private System.Windows.Forms.TextBox outputPathTextBox;
+    private System.Windows.Forms.Button outputPathButton;
+
+    private System.Windows.Forms.TextBox netAreaTextBox;
+    private System.Windows.Forms.Label netAreaLabel;
+
+    private System.Windows.Forms.TextBox netIdTextBox;
+
+    private System.Windows.Forms.Label netOwnerLabel;
+
+    private System.Windows.Forms.CheckBox timeSeries1;
+    private System.Windows.Forms.CheckBox timeSeries15;
+    private System.Windows.Forms.CheckBox consumtionAp;
+
+    private System.Windows.Forms.CheckBox productionAp;
+
+    private System.Windows.Forms.Label dateToLabel;
+    private System.Windows.Forms.DateTimePicker dateToPicker;
+
+    private System.Windows.Forms.DateTimePicker dateFromPicker;
+    private System.Windows.Forms.Label uniqueIdLabel;
+    private System.Windows.Forms.Label dateFromLabel;
+
+    private System.Windows.Forms.TextBox inputBox;
+
+    private System.Windows.Forms.TextBox uniqueIdTextBox;
+
+    #endregion
+}
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace UTILTSMaker;
+
+public partial class Form1 : Form
+{
+    string uniqueIdProperty => uniqueIdTextBox.Text;
+    string netAreaProperty => netAreaTextBox.Text;
+    string netOwnerProperty => netIdTextBox.Text;
+    string accountPointIDProperty => accountingPointTextBox.Text;
+    string dateToProperty => dateToPicker.Value.ToString("yyyyMMddHHmm");
+    string dateFromProperty => dateFromPicker.Value.ToString("yyyyMMddHHmm");
+    string timeNowProperty => DateTime.Now.ToString("yyyyMMddHHmm");
+    string documentTimeProperty => documentTimeFromNetTextBox.Text;
+    string meteringFromProperty => meteringFrom.Text;
+    string meteringToProperty => meteringTo.Text;
+    string meteringTotal => meteringTotalBox.Text;
+    
+    public Form1()
+    {
+        InitializeComponent();
+        uniqueIdTextBox.Text = "SEAB" + DateTime.Now.ToString("yyyyMMdd");
+        
+        dateFromPicker.Format = DateTimePickerFormat.Custom;
+        dateFromPicker.CustomFormat = "yyyy-MM-dd HH:mm";
+        dateFromPicker.ShowUpDown = false;
+
+        dateToPicker.Format = DateTimePickerFormat.Custom;
+        dateToPicker.CustomFormat = "yyyy-MM-dd HH:mm";
+        dateToPicker.ShowUpDown = false;
+
+    }
+   
+    private void timeSeries1_CheckedChanged(object sender, EventArgs e)
+    {
+        timeSeries15.Checked = false;
+        meteringTo.Visible = timeSeries1.Checked;
+        meteringFrom.Visible = timeSeries1.Checked;
+        meteringLabel.Visible = timeSeries1.Checked;
+        meteringTotalBox.Visible = timeSeries1.Checked;
+        inputBox.Enabled = !timeSeries1.Checked;
+    }
+
+    private void timeSeries15_CheckedChanged(object sender, EventArgs e)
+    {
+        timeSeries1.Checked = false;
+    }
+
+    private void consumtionAp_CheckedChanged(object sender, EventArgs e)
+    {
+        productionAp.Checked = false;
+    }
+
+    private void productionAp_CheckedChanged(object sender, EventArgs e)
+    { 
+        consumtionAp.Checked = false;
+    }
+
+    private void createFileButton_Click(object sender, EventArgs e)
+    {
+        string path = outputPathTextBox.Text;
+
+        if (path == "")
+        {
+            MessageBox.Show("Välj en utdata-katalog");
+        }
+        else
+        {
+            createTextFile(uniqueIdProperty, netAreaProperty, netOwnerProperty, path, dateToProperty, dateFromProperty, accountPointIDProperty, timeNowProperty, documentTimeProperty, meteringToProperty, meteringFromProperty, meteringTotal);
+            MessageBox.Show("Filen har skapats!");
+            outputPathTextBox.Text = "";
+        }
+    }
+
+    private void outputPathButton_Click(object sender, EventArgs e)
+    {
+
+        saveFileDialog1.FileName = $"UTILTS-E66-{uniqueIdProperty}-{accountPointIDProperty}-{netAreaProperty}-{netOwnerProperty}.uts";
+        saveFileDialog1.DefaultExt = "uts";
+        saveFileDialog1.AddExtension = true;
+        
+        if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+            outputPathTextBox.Text = saveFileDialog1.FileName;
+        }
+    }
+
+    private void uniqueIdTextBox_TextChanged(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void createTextFile(string uniqueId, string netArea, string netOwner, string path, string dateTo, string dateFrom, string accountPoint, string timeNow, string documentTime, string meteringTo, string meteringFrom, string meteringTotal)
+    {
+        string metering = "";
+        if (consumtionAp.Checked)
+        {
+            metering = "E17";
+        }else if (productionAp.Checked)
+        {
+            metering = "E18";
+        }
+
+        string values = inputBox.Text.Trim();
+        string[] valuesArray = values.Split(
+            new[] { '\r', '\n', '\t'},
+            StringSplitOptions.RemoveEmptyEntries
+            );
+        StringBuilder qtyLines = new StringBuilder();
+        for (int i = 0; i < valuesArray.Length; i++)
+        {
+            int seq = i * 4;
+            string value = valuesArray[i].Trim().Replace(',', '.');
+            qtyLines.AppendLine($"SEQ++{seq}'");
+            qtyLines.AppendLine($"QTY+136:{value}'");
+
+            qtyLines.AppendLine($"SEQ++{seq + 1}'");
+            qtyLines.AppendLine("QTY+136:0.00'");
+
+            qtyLines.AppendLine($"SEQ++{seq + 2}'");
+            qtyLines.AppendLine("QTY+136:0.00'");
+
+            qtyLines.AppendLine($"SEQ++{seq + 3}'");
+            qtyLines.AppendLine("QTY+136:0.00'");
+        }
+
+        if (documentTime == String.Empty)
+        {
+            documentTime = timeNow;
+        }
+        string content_timeSeries15 = $"""
+                                       UNA:+.? '
+                                       UNB+UNOC:3+{netOwner}:ZZ+49100:ZZ+260518:0823+{uniqueId}++23-DDQ-E66-T++1'
+                                       UNH+1+UTILTS:D:02B:UN:E5SE5A'
+                                       BGM+E66::260+{uniqueId}+9+AB'
+                                       DTM+137:{timeNow}:203'
+                                       DTM+735:?+0100:406'
+                                       MKS+23+E02::260'
+                                       NAD+MS+{netOwner}:SVK:260'
+                                       NAD+MR+49100:SVK:260'
+                                       NAD+DDQ'
+                                       IDE+24+{uniqueId}U0001'
+                                       LOC+172+{accountPoint}::9'
+                                       LOC+239+{netArea}:SVK:260'
+                                       LIN+++8716867000030:::9'
+                                       DTM+324:{dateFrom}{dateTo}:719'
+                                       DTM+597:{documentTime}:203'
+                                       DTM+354:15:806'
+                                       STS+7++E88::260'
+                                       MEA+AAZ++KWH'
+                                       CCI+++E12::260'
+                                       CAV+{metering}::260'
+                                       {qtyLines}
+                                       """;
+        string content_timeSeries1 = $"""
+                                      UNA:+.? '
+                                      UNB+UNOC:3+{netOwner}:ZZ+49100:ZZ+260522:1321+{uniqueId}++23-DDQ-E66-S++1'
+                                      UNH+1+UTILTS:D:02B:UN:E5SE0A'
+                                      BGM+E66::260+{uniqueId}+9+AB'
+                                      DTM+137:{timeNow}:203'
+                                      DTM+735:?+0100:406'
+                                      MKS+23+E02::260'
+                                      NAD+MR+49100:SVK:260'
+                                      NAD+MS+{netOwner}:SVK:260'
+                                      NAD+DDQ'
+                                      IDE+24+{uniqueId}'
+                                      LOC+172+{accountPoint}::9'
+                                      LOC+239+{netArea}:SVK:260'
+                                      LIN+++8716867000030:::9'
+                                      DTM+324:{dateFrom}{dateTo}:719'
+                                      DTM+597:{documentTime}:203'
+                                      DTM+354:1:802'
+                                      STS+7++E88::260'
+                                      MEA+AAZ++KWH'
+                                      CCI+++E12::260'
+                                      CAV+E17::260'
+                                      SEQ++1'
+                                      RFF+AES:101'
+                                      RFF+MG:697342847104340435'
+                                      QTY+220:{meteringFrom}'
+                                      DTM+597:{dateFrom}:203'
+                                      CCI+++E22::260'
+                                      CAV+E27::260'
+                                      SEQ++2'
+                                      RFF+AES:101'
+                                      QTY+220:{meteringTo}'
+                                      DTM+597:{dateTo}:203'
+                                      CCI+++E22::260'
+                                      CAV+E27::260'
+                                      SEQ++3'
+                                      QTY+136:{meteringTotal}'
+                                      """;
+        content_timeSeries15 = AddLineCountAsLastLine(content_timeSeries15, uniqueId);
+        content_timeSeries1 = AddLineCountAsLastLine(content_timeSeries1, uniqueId);
+        
+        if (timeSeries15.Checked && !timeSeries1.Checked)
+        {
+            File.WriteAllText(path, content_timeSeries15);
+        }else if (timeSeries1.Checked && !timeSeries15.Checked)
+        {
+            File.WriteAllText(path, content_timeSeries1);
+        }
+        else
+        {
+            MessageBox.Show("Välj en timserie");
+        }
+        
+    }
+    private string AddLineCountAsLastLine(string content, string uniqueId)
+    {
+        int lineCount = content
+            .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+            .Length;
+
+        return content + $"UNT+{lineCount - 1}+1'{Environment.NewLine}" + $"UNZ+1+{uniqueId}'";
+    }
+    private void UpdateMeteringTotal()
+    {
+        if (decimal.TryParse(meteringFrom.Text.Replace(',', '.'), out decimal from) &&
+            decimal.TryParse(meteringTo.Text.Replace(',', '.'), out decimal to))
+        {
+            decimal total = to - from;
+            meteringTotalBox.Text = total.ToString("0.##");
+        }
+        else
+        {
+            meteringTotalBox.Text = "";
+        }
+
+    }
+
+    private void meteringFrom_TextChanged(object sender, EventArgs e)
+    {
+        UpdateMeteringTotal();
+    }
+
+    private void meteringTo_TextChanged(object sender, EventArgs e)
+    {
+        UpdateMeteringTotal();
+    }
+}
